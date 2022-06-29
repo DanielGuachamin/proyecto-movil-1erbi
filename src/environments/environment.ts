2f32232c8b3545f8be80c4d9fd66819bae4bbc68
DanielGuachamin <guillermo.guachamin@epn.edu.ec>
2022-06-28 21:14:29 -0500

    Primera entrada de la app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'proyecto-appmovil-2022a',
+    appId: '1:728856571351:web:9f27f9d6f0aad93cb4ef92',
+    databaseURL: 'https://proyecto-appmovil-2022a-default-rtdb.firebaseio.com',
+    storageBucket: 'proyecto-appmovil-2022a.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBR_ywbrLfiRroZTvN37-omPgpHzIimS10',
+    authDomain: 'proyecto-appmovil-2022a.firebaseapp.com',
+    messagingSenderId: '728856571351',
+    measurementId: 'G-ZDSSPBYMC3',
+  },
   production: false
 };
 
